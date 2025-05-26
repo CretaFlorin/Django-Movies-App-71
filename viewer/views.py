bcac95427e2be8c57b0506b512b3d871e1ac0805
unknown <biancabianca36@yahoo.com>
2025-05-26 16:36:10 +0100

    Lista de filme

--- viewer/views.py
+++ viewer/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from viewer.models import Movie
 
 def main_page(request):
+    movies=Movie.objects.all()
     return render(
         request,
         template_name="main_page.html",
+        context={
+            "movies":movies
+        }
     )
